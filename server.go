6f9e238316581c791ef4f618fd590710dc4eb3cb
JP Robinson <john.paul.robinson@gmail.com>
2016-10-21 19:43:30 -0400

    adding retries, timeouts and warmups

--- server.go
+++ server.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/gorilla/mux"
+	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 
-	"github.com/gorilla/mux"
 	"github.com/jprobinson/gosubway"
 )
 
@@ -17,6 +19,8 @@ func init() {
 	key := os.Getenv("MTA_KEY")
 	r := mux.NewRouter()
 	r.HandleFunc("/svc/subway-api/v1/next-trains/{feed}/{stopID}", nextTrains(key)).Methods("GET")
+	r.HandleFunc("/_ah/warmup",
+		func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
 	http.Handle("/", r)
 }
 
@@ -35,10 +39,10 @@ func nextTrains(key string) http.HandlerFunc {
 		feedType := vars["feed"]
 
 		ctx := appengine.NewContext(r)
-		feed, err := gosubway.GetFeed(ctx, key, (feedType == ltrain))
+		feed, err := getFeed(ctx, key, (feedType == ltrain))
 		if err != nil {
-			log.Errorf(ctx, "unable to get subway feed: ", err)
-			http.Error(w, "dammit", http.StatusBadRequest)
+			log.Errorf(ctx, "unable to get subway feed: %s", err)
+			http.Error(w, "unable to read subway feed", http.StatusInternalServerError)
 			return
 		}
 
@@ -48,8 +52,37 @@ func nextTrains(key string) http.HandlerFunc {
 		if err != nil {
 			log.Errorf(ctx, "unable to encode response: ", err)
 		}
-
 	}
+}
+
+const (
+	timeout     = 1 * time.Second
+	maxAttempts = 10
+	backoffStep = 50 * time.Millisecond
+)
+
+// retries until it hits max attempts or a context timeout
+func getFeed(ctx context.Context, key string, l bool) (*gosubway.FeedMessage, error) {
+	var (
+		feed *gosubway.FeedMessage
+		err  error
+	)
+
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		// retry backoff
+		time.Sleep(time.Duration((attempt - 1)) * backoffStep)
+		// attempt to get feed
+		feed, err = gosubway.GetFeed(ctx, key, l)
+		if err == nil ||
+			(err != nil && strings.Contains(err.Error(), "deadline exceeded")) {
+			break
+		}
+		log.Errorf(ctx, "unable to get mta feed on attempt %d: %s", attempt, err)
+	}
+	return feed, err
 }
 
 type nextTrainResp struct {
